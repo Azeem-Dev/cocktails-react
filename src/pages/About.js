41d4c3a84f2db8e2212321e2a276e898c88b1b9a
Azeem-Dev <azeemsharif5822@gmail.com>
2020-12-06 22:58:03 +0500

    fetching data from api and showing cocktails list on the screen worked with useRef hook from react and using useCallback

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className='section-title'> About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo consequatur, culpa modi ea sapiente enim tempora? Ratione error illo adipisci necessitatibus, dignissimos nemo ab nam velit esse accusamus assumenda minima.
+      </p>
+    </section>
+  )
+}
+
+export default About
